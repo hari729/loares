9dcf29a6fa56d6ef38c3bd51326b3ee5fc8b7220
Hari Shankar <harishankar@tutamail.com>
2025-10-13 15:11:01 +0530

    removed random initialization from bwr_mod

--- algorithms/bmr_bwr.py
+++ algorithms/bmr_bwr.py
@@ -85,18 +85,13 @@ def bwr_mod(current_set,bounds):
     
     u = np.random.rand(pop_size,1)
     beta_mask = u > 0.5
-    c = 1/(20+1)
+    c = 1/(15+1)
     beta = np.where(beta_mask, ((1/(2*(1-u)))**c), (2*u)**c)
 
     r1 = 0.5*(beta+1)
     r2 = 0.5*(beta-1)
 
-    r3 = np.random.rand(pop_size, 1)
-    r4 = np.random.rand(pop_size, 1)
-
     T = np.random.choice([1, 2], size=(pop_size, 1))
-    
-    mask = r4 > 0.5
     # First compute all the term arrays
     best_pop = current_set.best
     worst_pop = current_set.worst
@@ -108,13 +103,6 @@ def bwr_mod(current_set,bounds):
                  r1  * (best_pop - T * random_pop) -
                  r2  * (worst_pop - random_pop))
 
-
-    # Compute random initialization for all positions
-    random_init = bounds[None, :, 1] - r3  * (bounds[None, :, 1] - bounds[None,:, 0])
-    
-    # Use numpy.where to choose between BWR and random based on mask
-    new = np.where(mask, new, random_init)    
-    
     new = np.clip(new,bounds[:,0],bounds[:,1])
 
     return new
