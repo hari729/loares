59aa1340d8f4cb0172a48f34db3c8ffc061400eb
Hari Shankar <harishankar@tutamail.com>
2025-11-09 12:30:02 +0530

    master list for each basefunction

--- core/process.py
+++ core/process.py
@@ -13,44 +13,39 @@ class ResultProcessor():
                  results_list,
                  root_dir = None):
         self.results = results_list
-        self.problem_info = self.results[0].problem.get_info()
         self.root_dir = Path(root_dir) if root_dir else Path(__main__.__file__).parent.resolve()
-        self.save_path = None
-        self.master_list_path = None
 
     def new_set(self, new_list):
         self.results = new_list
-        self.problem_info = self.results[0].problem.get_info()
-        self.algorithm_info = self.results[0].algorithm.get_info()
 
-    def set_path(self):
-        problem_name = self.problem_info['name']
-        algo_name = self.results[0].algorithm.get_info()["name"]
-        psize = self.problem_info['psize']
-        max_evals = self.problem_info['max_evals']
-        self.save_path = f"{self.root_dir}/results/{problem_name}/{algo_name}/{psize}_{max_evals}"
-        os.makedirs(self.save_path, exist_ok=True)
-        self.master_list_path = Path(f"{self.root_dir}/results/{problem_name}/master_list.csv")
+    def set_path(self, result):
+        problem_info = result.problem.get_info()
+        algo_info = result.algorithm.get_info()
+        psize = problem_info['psize']
+        max_evals = problem_info['max_evals']
+        seed = algo_info["seed"]
+        save_path = f"{self.root_dir}/results/{problem_info["name"]}/{algo_info["name"]}/{psize}_{max_evals}/{seed}"
+        os.makedirs(save_path, exist_ok=True)
+        master_list_path = Path(f"{self.root_dir}/results/{problem_info["name"]}/{algo_info["BaseFunction"]}_master_list.csv")
+        return problem_info, algo_info, save_path, master_list_path
 
     def generate_plots(self,*args):
         pass
 
     def generate_results(self):
-        self.set_path()
         master_list = []
         for result in self.results:
-            algorithm_info = result.algorithm.get_info()
-            save_path = f"{self.save_path}/{algorithm_info["seed"]}"
-            os.makedirs(save_path, exist_ok=True)
+            problem_info, algorithm_info, save_path, master_list_path = self.set_path(result)
 
-            master_list.append({"algorithm" : algorithm_info['name'],
+            master_list.append({"problem" : problem_info["name"],
+                                "algorithm" : algorithm_info['name'],
                                 "seed" : algorithm_info['seed'],
-                                "psize" : self.problem_info["psize"],
-                                "max_evals" : self.problem_info["max_evals"],
+                                "psize" : problem_info["psize"],
+                                "max_evals" : problem_info["max_evals"],
                                 **result.final_metrics})
 
             with open(f"{save_path}/problem_settings.json", "w") as write_file:
-                json.dump(self.problem_info, write_file)
+                json.dump(problem_info, write_file)
 
             with open(f"{save_path}/algorithm_settings.json", "w") as write_file:
                 json.dump(algorithm_info, write_file)
@@ -65,7 +60,10 @@ class ResultProcessor():
 
 
         mf = pd.DataFrame(master_list)
-        if self.master_list_path.exists():
-            mf.to_csv(self.master_list_path, mode='a', header=False, index = False)
+        if master_list_path.exists():
+            existing = pd.read_csv(master_list_path)
+            combined = pd.concat([existing, mf], ignore_index=True)
+            combined.drop_duplicates(subset=["psize", "max_evals", "seed"], keep="last", inplace=True)
+            combined.to_csv(master_list_path, index=False)
         else:
-            mf.to_csv(self.master_list_path, mode='w', header=True, index = False)
+            mf.to_csv(master_list_path, mode='w', header=True, index = False)
